c9280529310a39e7d2ff382f914f941e7cefed28
Vladimir Malikov <v.malikov.hamburg@gmail.com>
2015-04-04 20:42:44 +0200

    HBQ angefangen.

--- Aufgabe1/Code/hbq.erl
+++ Aufgabe1/Code/hbq.erl
@@ -1,1 +1,22 @@
 -module (hbq).
+
+start()->
+ not_imp.
+
+loop()->
+ not_imp.
+
+ initHBQandDLQ(ServerPID)->
+ not_imp.
+
+ pushHBQ(ServerPID, OldHBQ, [NNr, Msg,TSclientout]) ->
+ not_imp.
+
+ deliverMSG(ServerPID, DLQ, NNr, ToClient)->
+ not_imp.
+
+ dellHBQ(ServerPID)->
+ not_imp.
+
+ pushSeries(HBQ, DLQ)->
+ not_imp.
