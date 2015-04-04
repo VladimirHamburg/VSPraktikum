aad13114ba9831a2d12b8c7bab392f20b86b04c5
Vladimir Malikov <v.malikov.hamburg@gmail.com>
2015-04-04 12:44:06 +0200

    TestCode angepasst.

--- Aufgabe1/Code/TestCode/chatSen.erl
+++ Aufgabe1/Code/TestCode/chatSen.erl
@@ -1,8 +1,10 @@
 -module (chatSen).
 -export ([startS/1]).
+-import (chatRec, [startR/0]).
 
 startS(Other) ->
 	register(chatS,self()),
+	spawn(chatRec,startR,[]),
 	loop(Other).
 
 loop(Other) ->
--- Aufgabe1/Code/cmem.erl
+++ Aufgabe1/Code/cmem.erl
@@ -16,7 +16,7 @@ getClientNNr({RemTime,ClientList},ClientID) ->
 delExpiredCl({RemTime,ClientList},Clientlifetime) ->
 	{RemTime,[{ClientID,NNr,TimeStamp} || {ClientID,NNr,TimeStamp} <- ClientList, checkTime(RemTime,TimeStamp)]}.
 
-%%%%%%
+%%%%%%%%%%%%Hielfsmethoden, nicht in der Dokumentation beschrieben
 updateClient_([],ClientID,NNr) ->
 	[{ClientID,NNr,erlang:now()}];
 updateClient_([{SavedID,_,_}|T],ClientID,NNr) when SavedID == ClientID ->
