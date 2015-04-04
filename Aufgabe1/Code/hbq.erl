bfda2b53cb6f09e698b9993660079ff6fd4e4dea
Vladimir Malikov <v.malikov.hamburg@gmail.com>
2015-04-04 23:58:18 +0200

    HBQ weiterentwickelt.

--- Aufgabe1/Code/hbq.erl
+++ Aufgabe1/Code/hbq.erl
@@ -6,11 +6,21 @@ start()->
 loop()->
  not_imp.
 
-initHBQandDLQ(ServerPID)->
- not_imp.
+initHBQandDLQ(ServerPID,Size)->
+ HBQandDLQ = {[],dlq:initDLQ(Size,"DLQLog")},
+ ServerPID ! {reply, ok}.
 
-pushHBQ(ServerPID, OldHBQ, [NNr, Msg,TSclientout]) ->
- not_imp.
+pushHBQ(ServerPID, {HBQ,DLQ}, [NNr, Msg,TSclientout]) ->
+ Flag = dlq:expectedNr(DLQ) == NNr,
+ {NewNBQ,NewDLQ} = case Flag of
+ 	true -> 
+ 		{HBQ,dlq:push2DLQ([NNr, Msg,TSclientout,erlang:now()])};
+ 	false ->
+ 		{pushToHBQ(HBQ,[NNr, Msg,TSclientout]),DLQ}
+ 	end,
+ ServerPID ! {reply, ok},
+ {NewNBQ,NewDLQ}.
+
 
 deliverMSG(ServerPID, DLQ, NNr, ToClient)->
  not_imp.
@@ -20,3 +30,8 @@ dellHBQ(ServerPID)->
 
 pushSeries(HBQ, DLQ)->
  not_imp.
+
+%%%%%%%%%%%%Hielfsmethoden, nicht in der Dokumentation beschrieben.
+
+pushToHBQ(HBQ,[NNr,Msg,TSclientout]) ->
+	[[WLNNr,WLMsg,WLTSclientout,WLTShbqin]|| [WLNNr,WLMsg,WLTSclientout,WLTShbqin] <- HBQ,WLNNr =< NNr] ++ [[NNr,Msg,TSclientout,erlang:now()]] ++ [[WRNNr,WRMsg,WRTSclientout,WRTShbqin]|| [WRNNr,WRMsg,WRTSclientout,WRTShbqin] <- HBQ,WRNNr > NNr].
