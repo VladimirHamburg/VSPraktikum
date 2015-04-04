471638d51e5e5ddcfa74a772cd9ed49c3309940f
piranha771 <piranha771@gmail.com>
2015-04-04 17:53:24 +0200

    DLQ: pushDLQ fixed. initDLQ, expectedNr und pushDLQ fertig

--- Aufgabe1/Code/dlq.erl
+++ Aufgabe1/Code/dlq.erl
@@ -2,22 +2,24 @@
 -export ([initDLQ/2,expectedNr/1,push2DLQ/3,deliverMSG/4]).
 
 initDLQ(Size, Datei) ->
-	{0, []}.
+	{Size, []}.
 
-expectedNr({_,Liste}) ->
-	expectedNr_(Liste).
+expectedNr({_,List}) ->
+	expectedNr_(List).
 
-push2DLQ(NewEntry, Queue, Datei) ->
-	Queue++NewEntry.
+push2DLQ(NewEntry, {Size,List}, Datei) when length(List) < Size ->
+	{Size, List++[NewEntry]};
+push2DLQ(NewEntry, {Size,[_|ListTail]}, Datei) ->
+	{Size, ListTail ++ [NewEntry]}.
 
 deliverMSG(MSGNr, ClientPID, Queue, Datei) ->
-.
+	lol.
 
 %%%%%%%% Hilfsmethoden, nicht in der Dokumentation beschrieben
 
 expectedNr_([]) ->
-	1.
-expectedNr_([{NNr, Msg, TSclientout, TShbqin}|[]]) ->
-	NNr+1.
-expectedNr_([H|T]) ->
+	1;
+expectedNr_([[NNr, _, _, _]|[]]) ->
+	NNr+1;
+expectedNr_([_|T]) ->
 	expectedNr_(T).
