3e013d8d71b9ef7621abb1a4dafdae2c39f7237e
piranha771 <piranha771@gmail.com>
2015-04-05 14:34:16 +0200

    Server:  start() soweit fertig, ungetestet; loop() fehlt timeout WIP

--- Aufgabe1/Code/server.erl
+++ Aufgabe1/Code/server.erl
@@ -2,21 +2,57 @@
 -export ([start/0]).
 
 start() ->
-	notImplemented.
+	{Latency, Clientlifetime, ServerName, HBQname, HBQnode, DLQlimit} = readConfig(),
+	registerServer(ServerName),
+	CMEM = initCMEM(RemTime, "cmem.log"), %% TODO: Init-Reihenfolge ok?
+	case initHBQ(HBQname, HBQnode) of
+		ok -> loop(Latency, Clientlifetime, ServerName, HBQname, HBQnode, DLQlimit, CMEM);
+		false -> {error, "Received bad response from initHBQ"}
+	end.
 
 readConfig() ->
-	notImplemented.
+	ServerConfigFile = "server.cfg",
+	Config = file:consult(ServerConfigFile),
+	{ok, Latency} = get_config_value(latency, Config),
+	{ok, Clientlifetime} = get_config_value(clientlifetime,Config),
+	{ok, ServerName} = get_config_value(servername,Config),
+	{ok, HBQname} = get_config_value(hbqname,Config),
+	{ok, HBQnode} = get_config_value(hbqnode,Config),
+	{ok, DLQlimit} = get_config_value(dlqlimit,Config),
+	{Latency, Clientlifetime, ServerName, HBQname, HBQnode, DLQlimit}.
 
-loop(Latency, Clientlifetime, Servername, HBQname, HBQnode, DLQlimit) ->
-	notImplemented.
+loop(Latency, Clientlifetime, Servername, HBQname, HBQnode, DLQlimit, CMEM) ->
+	cmem:delExpiredCl(CMEM, Clientlifetime),
+	receive 
+		{ClientPID, getmessages} -> sendMessages(ClientPID, CMEM);
+		{dropmessage, [INNr, Msg, TSclientout]} -> dropmessage(HBQname, HBQnode);
+		{ClientPID, getmsgid} -> sendMSGID(ClientPID)
+	end,
+	loop().
 
 sendMessages(ToClient, CMEM) ->
-	notImplemented.
+	cmem:getClientNNr(CMEM, ToClient).
 
 dropmessage(HBQname, HBQnode) ->
 	notImplemented.
 
 sendMSGID(ClientPID, CMEM) ->
 	notImplemented.
+
+%%%%%%%%%%%%%%%%% Interne Hilfsmethoden
+
+initCMEM(Clientlifetime, CMEMLogFile)
+	cmem:initCMEM(Clientlifetime, CMEMLogFile).
+
+registerServer(ServerName)
+	register(ServerName, self()).
+
+initHBQ(HBQname, HBQnode) ->
+	{HBQname,HBQnode} ! {self(), request, initHBQ},
+	receive 
+		{reply, ok} -> ok
+		{reply} -> fail
+	end.
+	
 
 
