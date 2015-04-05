94a1f593e0ef7b34b790d9b21115e528516ad058
piranha771 <piranha771@gmail.com>
2015-04-05 11:34:47 +0200

    server: funktions header eingefügt

---
+++ Aufgabe1/Code/server.erl
@@ -0,0 +1,22 @@
+-module (server).
+-export ([start/0]).
+
+start() ->
+	notImplemented.
+
+readConfig() ->
+	notImplemented.
+
+loop(Latency, Clientlifetime, Servername, HBQname, HBQnode, DLQlimit) ->
+	notImplemented.
+
+sendMessages(ToClient, CMEM) ->
+	notImplemented.
+
+dropmessage(HBQname, HBQnode) ->
+	notImplemented.
+
+sendMSGID(ClientPID, CMEM) ->
+	notImplemented.
+
+
