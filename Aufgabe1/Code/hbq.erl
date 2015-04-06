6f6fa34c775026873a196be799593a2c0ef8e821
Vladimir Malikov <v.malikov.hamburg@gmail.com>
2015-04-07 00:03:23 +0200

    Logging eingebaut. Nachrichten die zu kleinen Nummer haben werden jetzt verworfen.

--- Aufgabe1/Code/hbq.erl
+++ Aufgabe1/Code/hbq.erl
@@ -1,6 +1,8 @@
 -module (hbq).
 -export ([start/0]).
 
+-define (LOG, werkzeug:to_String(erlang:node()) ++ ".log").
+
 start()->
 	{ok, ConfigListe} = file:consult("server.cfg"),
 	{ok,HBQname} = werkzeug:get_config_value(hbqname,ConfigListe),
@@ -9,6 +11,7 @@ start()->
 	io:fwrite("HBQ bereit. \n"),
  receive
  	{ServerPID, {request,initHBQ}} ->
+ 		werkzeug:logging(?LOG, "HBQ und DLQ gestartet. \n"),
  		loop(initHBQandDLQ(ServerPID,DLQlimit))
  end.
 
@@ -32,13 +35,23 @@ initHBQandDLQ(ServerPID,Size)->
 
 pushHBQ(ServerPID, {HBQ,DLQ}, [NNr, Msg,TSclientout]) ->
  Flag = dlq:expectedNr(DLQ) == NNr,
+ werkzeug:logging(?LOG, werkzeug:to_String(NNr) ++ "te Nachricht angekommen. Erwartet wird: "++ werkzeug:to_String(dlq:expectedNr(DLQ)) ++" \n"),
  {NewNBQ,NewDLQ} = case Flag of
- 	true -> 
+ 	true ->
+ 		werkzeug:logging(?LOG, werkzeug:to_String(NNr) ++ " direkt in DLQ gespeichert. \n"), 
  		{HBQ,dlq:push2DLQ([NNr, Msg,TSclientout,erlang:now()],DLQ,"DLQLog")};
  	false ->
- 		NewHBQ = pushToHBQ(HBQ,[NNr, Msg,TSclientout]),
- 		[[LNNr,_,_,_]|_] = NewHBQ,
- 		pushSeries(NewHBQ,DLQ,dlq:expectedNr(DLQ),LNNr)
+ 		Flag2 = dlq:expectedNr(DLQ) > NNr,
+ 		case Flag2 of
+ 			false ->
+ 				NewHBQ = pushToHBQ(HBQ,[NNr, Msg,TSclientout]),
+				werkzeug:logging(?LOG, "Nachricht " ++ werkzeug:to_String(NNr) ++ " in HBQ gespeichert.\n"),
+ 				[[LNNr,_,_,_]|_] = NewHBQ,
+ 				pushSeries(NewHBQ,DLQ,dlq:expectedNr(DLQ),LNNr);
+ 			true ->
+ 				werkzeug:logging(?LOG, "Nachrichten "++ werkzeug:to_String(NNr) ++ " verworfen. \n"),
+ 				{HBQ,DLQ}		
+ 		end
  	end,
  ServerPID ! {reply, ok},
  {NewNBQ,NewDLQ}.
@@ -51,7 +64,8 @@ deliverMSG(ServerPID, DLQ, NNr, ToClient)->
 dellHBQ(ServerPID)->
  ServerPID ! {reply, ok}.
 
-pushSeries(HBQ, {Size,DLQList},ExNNr,NNr) when (ExNNr+1) == NNr ->
+pushSeries(HBQ, {Size,DLQList},ExNNr,NNr) when ExNNr == NNr ->
+	werkzeug:logging(?LOG, "Nachrichten aus HBQ können ohne Lücke in DLQ gespeichert werden \n"),
 	InList = pushSeries_findall(HBQ,NNr,[]),
 	{_,NewHBQ} = lists:split(length(InList),HBQ),
 	NewDLQ = pushSeries_DLQ(InList,{Size,DLQList}),
@@ -60,8 +74,10 @@ pushSeries(HBQ, {Size,DLQList},ExNNr,NNr) when length(HBQ) > Size*(2/3) ->
 	InList = pushSeries_findall(HBQ,NNr,[]),
 	{_,NewHBQ} = lists:split(length(InList),HBQ),
 	[[LNNr,_,_,_]|_] = InList,
-	ErrMsg = [ExNNr,"Fehlernachricht für Nachrichtennummern " ++ werkzeug:to_String(ExNNr) ++ " bis " ++ werkzeug:to_String(LNNr-1),erlang:now(),erlang:now()],
+	NewErrMsg = "Fehlernachricht für Nachrichtennummern " ++ werkzeug:to_String(ExNNr) ++ " bis " ++ werkzeug:to_String(LNNr-1),
+	ErrMsg = [ExNNr,NewErrMsg,erlang:now(),erlang:now()],
 	NewDLQ = pushSeries_DLQ([ErrMsg] ++ InList,{Size,DLQList}),
+	werkzeug:logging(?LOG, NewErrMsg ++"\n"),
 	{NewHBQ,NewDLQ};
 pushSeries(HBQ, DLQ,ExNNr,NNr) ->
 	{HBQ,DLQ}.
