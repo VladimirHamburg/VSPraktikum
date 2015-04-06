0856524db279563553a2d29d0efdeea271b92333
piranha771 <piranha771@gmail.com>
2015-04-06 12:25:02 +0200

    Resolved merge conflicts

--- Aufgabe1/Code/server.erl
+++ Aufgabe1/Code/server.erl
@@ -2,13 +2,6 @@
 -export ([start/0]).
 
 start() ->
-<<<<<<< HEAD
-	{Latency, Clientlifetime, ServerName, HBQname, HBQnode, DLQlimit} = readConfig(),
-	registerServer(ServerName),
-	CMEM = initCMEM(RemTime, "cmem.log"), %% TODO: Init-Reihenfolge ok?
-	case initHBQ(HBQname, HBQnode) of
-		ok -> loop(Latency, Clientlifetime, ServerName, HBQname, HBQnode, DLQlimit, CMEM);
-=======
 	io:fwrite("Server starting up...\n"),
 	{Latency, Clientlifetime, ServerName, HBQname, HBQnode, DLQlimit} = readConfig(),
 	registerServer(ServerName),
@@ -17,58 +10,11 @@ start() ->
 		ok ->
 			io:fwrite("Server startup complete!\n"),
 			loop(Latency, Clientlifetime, ServerName, HBQname, HBQnode, DLQlimit, CMEM, werkzeug:reset_timer(null, Latency, stop),1);
->>>>>>> master
 		false -> {error, "Received bad response from initHBQ"}
 	end.
 
 readConfig() ->
 	ServerConfigFile = "server.cfg",
-<<<<<<< HEAD
-	Config = file:consult(ServerConfigFile),
-	{ok, Latency} = get_config_value(latency, Config),
-	{ok, Clientlifetime} = get_config_value(clientlifetime,Config),
-	{ok, ServerName} = get_config_value(servername,Config),
-	{ok, HBQname} = get_config_value(hbqname,Config),
-	{ok, HBQnode} = get_config_value(hbqnode,Config),
-	{ok, DLQlimit} = get_config_value(dlqlimit,Config),
-	{Latency, Clientlifetime, ServerName, HBQname, HBQnode, DLQlimit}.
-
-loop(Latency, Clientlifetime, Servername, HBQname, HBQnode, DLQlimit, CMEM) ->
-	cmem:delExpiredCl(CMEM, Clientlifetime),
-	receive 
-		{ClientPID, getmessages} -> sendMessages(ClientPID, CMEM);
-		{dropmessage, [INNr, Msg, TSclientout]} -> dropmessage(HBQname, HBQnode);
-		{ClientPID, getmsgid} -> sendMSGID(ClientPID)
-	end,
-	loop().
-
-sendMessages(ToClient, CMEM) ->
-	cmem:getClientNNr(CMEM, ToClient).
-
-dropmessage(HBQname, HBQnode) ->
-	notImplemented.
-
-sendMSGID(ClientPID, CMEM) ->
-	notImplemented.
-
-%%%%%%%%%%%%%%%%% Interne Hilfsmethoden
-
-initCMEM(Clientlifetime, CMEMLogFile)
-	cmem:initCMEM(Clientlifetime, CMEMLogFile).
-
-registerServer(ServerName)
-	register(ServerName, self()).
-
-initHBQ(HBQname, HBQnode) ->
-	{HBQname,HBQnode} ! {self(), request, initHBQ},
-	receive 
-		{reply, ok} -> ok
-		{reply} -> fail
-	end.
-	
-
-
-=======
 	{ok, Config} = file:consult(ServerConfigFile),
 	{ok, Latency} = werkzeug:get_config_value(latency, Config),
 	{ok, Clientlifetime} = werkzeug:get_config_value(clientlifetime,Config),
@@ -146,4 +92,3 @@ terminateHBQ(HBQname, HBQnode) ->
 		_ -> 
 			fail
 	end.
->>>>>>> master
