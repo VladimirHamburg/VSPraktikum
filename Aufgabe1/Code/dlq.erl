6e1c7a8bb7b7d9bc752e9ec024cc3047cb180ff3
piranha771 <piranha771@gmail.com>
2015-04-07 21:27:57 +0200

    dlq: added logging server: removed logErrorRet function

--- Aufgabe1/Code/dlq.erl
+++ Aufgabe1/Code/dlq.erl
@@ -2,23 +2,30 @@
 -export ([initDLQ/2,expectedNr/1,push2DLQ/3,deliverMSG/4]).
 
 initDLQ(Size, Datei) ->
+	log("DLQ initialized", Datei),
 	{Size, []}.
 
 expectedNr({_,List}) ->
 	expectedNr_(List).
 
 push2DLQ(NewEntry, {Size,List}, Datei) when length(List) < Size ->
+	[MSGNr|_] = NewEntry,
+	log("DLQ adds "++werkzeug:to_String(MSGNr), Datei),
 	{Size, List ++ makeEntry(NewEntry)};
 push2DLQ(NewEntry, {Size,[_|ListTail]}, Datei) ->
+	[MSGNr|_] = NewEntry,
+	log("DLQ shifts "++werkzeug:to_String(MSGNr), Datei),
 	{Size, ListTail ++ makeEntry(NewEntry)}.
 
 deliverMSG(MSGNr, ClientPID, {_, List}, Datei) ->
+	log("DLQ should send "++werkzeug:to_String(MSGNr), Datei),
 	case MSGNr > 0 of
 		true ->
 			case MSGNr < expectedNr_(List) of
 				true -> 
 					deliverMSG_(MSGNr, ClientPID, List, List, Datei);
 				false ->
+					log("DLQ sends "++werkzeug:to_String(MSGNr-1), Datei),
 					ClientPID ! {reply,[MSGNr-1, "Nicht leere Dummy Nachricht", erlang:now(), erlang:now(), erlang:now(), erlang:now()], true},
 					MSGNr-1
 			end;
@@ -44,7 +51,11 @@ makeEntry([NNr, Msg, TSclientout, TShbqin]) ->
 deliverMSG_(MSGNr, ClientPID, [], BuList, Datei) -> %% Nicht gefunden? Erneut suchen mit größerer nummer
 	deliverMSG_(MSGNr+1, ClientPID, BuList, BuList, Datei);
 deliverMSG_(MSGNr, ClientPID, [[NNr, Msg, TSclientout, TShbqin, TSdlqin]|_], BuList, Datei) when MSGNr == NNr ->
+	log("DLQ sends "++werkzeug:to_String(MSGNr), Datei),
 	ClientPID ! {reply,[NNr, Msg, TSclientout, TShbqin, TSdlqin, erlang:now()], MSGNr > expectedNr_(BuList)-1},
 	MSGNr;
 deliverMSG_(MSGNr, ClientPID, [_|Tail], BuList, Datei) ->
 	deliverMSG_(MSGNr, ClientPID, Tail, BuList, Datei).
+
+log(Msg, Datei) ->
+	werkzeug:logging(Datei, Msg++"\n").
