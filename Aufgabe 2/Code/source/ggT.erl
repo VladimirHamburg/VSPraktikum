d996ee92c1e3bec9b33d658383b41ec4a4594da2
Vladimir Malikov <v.malikov.hamburg@gmail.com>
2015-05-05 08:58:11 +0200

    ggT/starter fertig, nicht getestet

--- Aufgabe 2/Code/source/ggT.erl
+++ Aufgabe 2/Code/source/ggT.erl
@@ -1,38 +1,88 @@
 -module (ggT).
 -export ([start/7]).
 
-start(Nameservice,Coordinator,NummerPgTm,DelayTime,TermTime,ID,StarterID) ->
+start(NameserviceName,Coordinator,{PrGr,Team,IDggT,StarterID},WorkTime,Quota,TermTime,ID) ->
+	Nameservice = global:whereis_name(NameserviceName),
 	%%Registriert sich an der Erlang-Node.
 	erlang:register(ID, erlang:self()),
 	%%Registriert sich beim Nameservice.
 	registerByNS(Nameservice,ID),
 	%%Meldet sich beim Koordinator
-	CoordinatorContacts = askForCoord(Nameservice,Coordinator),
+	CoordinatorContacts = askForCoord(Nameservice,Coordinator,ID),
 	registerByCoor(CoordinatorContacts,ID),
 	%%Wartet auf die Setzung der Namchbarn
-	NBors = waitForNBors(),
-	{LeftN,RightN} = getNBorsContacts(Nameservice,NBors),
+	NBors = waitForNBors(Nameservice,ID),
+	{LeftN,RightN} = getNBorsContacts(Nameservice,NBors,ID),
 	%%Wartet auf initialen Mi-Wert
-	Mi = waitForMi().
+	Mi = waitForMi(Nameservice,ID),
+	loop(Mi, ID, Nameservice, CoordinatorContacts, {LeftN,RightN}, {PrGr,Team,ID,StarterID}, WorkTime,TermTime,"LogFile",0,Quota,werkzeug:reset_timer(ID, TermTime/2, vote)).
 
 %%Alle Methoden ab hier wurden im Entwurf vorgegebn.
 
 %%%% loop()
+loop(Mi, ID, Nameservice, Coordinator, _, AllIDData, _,_,LogFile,VoteFlag,Quota,_) when VoteFlag == 2 ->
+	vote(Mi, ID, Nameservice, Coordinator, AllIDData, LogFile, Quota, 0);
+loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFile,VoteFlag,Quota,Timer) ->
+	receive
+		{setpm,MiNeu} ->
+			werkzeug:reset_timer(Timer, TermTime/2, vote),
+			loop(MiNeu, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,0,Quota,Timer);
+		{sendy,Y} ->
+			werkzeug:reset_timer(Timer, TermTime/2, vote),
+			MiNeu = calcNewMi(Y,Mi,Coordinator,NBors,ID,WorkTime),
+			loop(MiNeu, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,0,Quota,Timer);
+		{From,{vote,Initiator}} ->
+			case VoteFlag of
+				1 ->
+					From ! {voteYes,ID},
+					loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,VoteFlag,Quota,Timer);
+				_ ->
+					loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,VoteFlag,Quota,Timer)
+			end;
+		{From,tellmi} ->
+			From ! {mi,Mi},
+			loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,VoteFlag,Quota,Timer);
+		{From,pingGGT} ->
+			From ! {pongGGT, ID},
+			loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,VoteFlag,Quota,Timer);
+		vote ->
+			werkzeug:reset_timer(Timer, TermTime/2, vote),
+			loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,VoteFlag+1,Quota,Timer);
+		kill ->
+			kill(Nameservice,ID)
+	end.
+
 %%%% vote()
+vote(Mi, ID, Nameservice, Coordinator, AllIDData, LogFile, Quota, YesQuota) when Quota == YesQuota ->
+	Coordinator ! {erlang:self(),briefterm,{ID,Mi,werkzeug:timeMilliSecond()}},
+	receive
+		kill ->
+			kill(Nameservice,ID)
+	end;
+vote(Mi, ID, Nameservice, Coordinator, AllIDData, LogFile, Quota, YesQuota) ->
+	Nameservice ! {erlang:self(),{multicast,vote,ID}},
+	receive
+		{voteYes, CName} ->
+			vote(Mi, ID, Nameservice, Coordinator, AllIDData, LogFile, Quota, YesQuota+1);
+		kill ->
+			kill(Nameservice,ID)
+	end.
 %%%% kill()
-kill() ->
-	ok.
+kill(Nameservice,ID) ->
+	Nameservice ! {erlang:self(),{unbind,ID}},
+	erlang:unregister(ID),
+	erlang:exit(erlang:self(),normal).
 %%HILFSMETHODEN%%
 
-askForCoord(Nameservice,Coordinator) ->
+askForCoord(Nameservice,Coordinator,ID) ->
 	Nameservice ! {erlang:self() ,{lookup,Coordinator}},
 	receive
 		{pin,{Name,Node}} ->
 			{Name,Node};
 		not_found ->
-			kill();
+			kill(Nameservice,ID);
 		kill ->
-			kill()
+			kill(Nameservice,ID)
 	end.
 
 registerByNS(Nameservice,ID) ->
@@ -41,40 +91,56 @@ registerByNS(Nameservice,ID) ->
 		ok ->
 			ok;
 		kill ->
-			kill()
+			kill(Nameservice,ID)
 	end.
 
-waitForNBors() ->
+waitForNBors(Nameservice,ID) ->
 	receive
 		{setneighbors,LeftN,RightN} ->
 			{LeftN,RightN};
 		kill ->
-			kill()
+			kill(Nameservice,ID)
 	end.
 
-getNBorsContacts(Nameservice,{LeftN,RightN}) ->
-{getNBorsContacts(Nameservice,LeftN),getNBorsContacts(Nameservice,RightN)};
-getNBorsContacts(Nameservice, NBor) ->
+getNBorsContacts(Nameservice,{LeftN,RightN},ID) ->
+{getNBorsContacts(Nameservice,LeftN,ID),getNBorsContacts(Nameservice,RightN,ID)};
+getNBorsContacts(Nameservice, NBor,ID) ->
 	Nameservice ! {erlang:self() ,{lookup,NBor}},
 	receive
 		{pin,{Name,Node}} ->
 			{Name,Node};
 		not_found ->
-			kill();
+			kill(Nameservice,ID);
 		kill ->
-			kill()
+			kill(Nameservice,ID)
 	end.
 
-waitForMi() ->
+waitForMi(Nameservice,ID) ->
 	receive
 		{setpm,MiNeu} ->
 			MiNeu;
 		kill ->
-			kill()
+			kill(Nameservice,ID)
 	end.
 
 registerByCoor(CoordinatorContacts,ID) ->
 	CoordinatorContacts ! {hello, ID}.
+
+calcNewMi(Y,Mi,Coordinator,NBors,ID,WorkTime) ->
+	NewMi = calc(Y,Mi),
+	time:sleep(WorkTime),
+	case NewMi of
+		nope ->
+				Mi;
+		_ ->
+			newMiSend(Mi, Coordinator, NBors, ID),
+			NewMi
+	end.
+
+newMiSend(Mi, Coordinator, {LeftN,RightN},ID) ->
+	Coordinator ! {briefmi,{ID,Mi,werkzeug:timeMilliSecond()}},
+	LeftN ! {sendy, Mi},
+	RightN ! {sendy, Mi}.
 
 calc(OtherMi,SelfMI) when OtherMi >= SelfMI ->
 	noop;
--- Aufgabe 2/Code/source/starter.erl
+++ Aufgabe 2/Code/source/starter.erl
@@ -1,1 +1,36 @@
 -module (starter).
+-export ([start/1]).
+
+start(Nr) ->
+	{Praktikumsgruppe,Teamnummer,Nameservicenode,Nameservicename,Koordinatorname} = readConfig(),
+	net_adm:ping(Nameservicenode),
+	Nameservice = global:whereis_name(Nameservicename),
+	Coordinator = askForCoord(Nameservice, Koordinatorname),
+	Coordinator ! {erlang:self(),getsteeringval},
+	receive
+		{steeringval,WorkTime,TermTime,Quota,GGTProzessnummer} ->
+			startGGTs(Nameservicename,Koordinatorname,{Praktikumsgruppe,Teamnummer,0,Nr},WorkTime,Quota,TermTime,erlang:list_to_atom(lists:concat([Praktikumsgruppe,Teamnummer,0,Nr])), GGTProzessnummer)
+	end.
+
+
+readConfig() ->
+	{ok, ConfigListe} = file:consult("ggt.cfg"),
+	{ok,Praktikumsgruppe} = werkzeug:get_config_value(praktikumsgruppe,ConfigListe),
+	{ok,Teamnummer} = werkzeug:get_config_value(teamnummer,ConfigListe),
+	{ok,Nameservicenode} = werkzeug:get_config_value(nameservicenode,ConfigListe),
+	{ok,Nameservicename} = werkzeug:get_config_value(nameservicename,ConfigListe),
+	{ok,Koordinatorname} = werkzeug:get_config_value(koordinatorname,ConfigListe),
+	{Praktikumsgruppe,Teamnummer,Nameservicenode,Nameservicename,Koordinatorname}.
+
+askForCoord(Nameservice,Coordinator) ->
+	Nameservice ! {erlang:self() ,{lookup,Coordinator}},
+	receive
+		{pin,{Name,Node}} ->
+			{Name,Node}
+	end.
+
+startGGTs(_,_,_,_,_,_,_,0) ->
+	ok;
+startGGTs(NameserviceName,Coordinator,{PrGr,Team,IDggT,StarterID},WorkTime,Quota,TermTime,ID, GGTProzessnummer) ->
+	erlang:spawn(ggt, start, [NameserviceName,Coordinator,{PrGr,Team,ID,StarterID},WorkTime,Quota,TermTime,ID]),
+	startGGTs(NameserviceName,Coordinator,{PrGr,Team,IDggT+1,StarterID},WorkTime,Quota,TermTime,erlang:list_to_atom(lists:concat([PrGr,Team,IDggT+1,StarterID])), GGTProzessnummer-1).
