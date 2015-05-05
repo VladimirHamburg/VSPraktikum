3cc25e849b0f1dc184885b06b13dbdd218587b8a
VladimirHamburg <v.malikov.hamburg@gmail.com>
2015-05-05 17:07:26 +0200

    ggT weiter gemacht.

--- Aufgabe 2/Code/source/ggT.erl
+++ Aufgabe 2/Code/source/ggT.erl
@@ -1,34 +1,45 @@
 -module (ggT).
 -export ([start/7]).
 
+-define (LOG, '../log/').
+
 start(NameserviceName,Coordinator,{PrGr,Team,IDggT,StarterID},WorkTime,Quota,TermTime,ID) ->
 	Nameservice = global:whereis_name(NameserviceName),
+	log(ID, t_s(ID) ++ "Startzeit: " ++ t_s(werkzeug:timeMilliSecond()) ++ " mit PID " ++ t_s(erlang:self())),
 	%%Registriert sich an der Erlang-Node.
 	erlang:register(ID, erlang:self()),
 	%%Registriert sich beim Nameservice.
 	registerByNS(Nameservice,ID),
+	log(ID, t_s(ID) ++ "beim Namensdienst und auf Node lokal registriert."),
 	%%Meldet sich beim Koordinator
 	CoordinatorContacts = askForCoord(Nameservice,Coordinator,ID),
 	registerByCoor(CoordinatorContacts,ID),
+	log(ID, t_s(ID) ++ "beim Koordinator gemeldet."),
 	%%Wartet auf die Setzung der Namchbarn
-	NBors = waitForNBors(Nameservice,ID),
-	{LeftN,RightN} = getNBorsContacts(Nameservice,NBors,ID),
+	{NBorsL,NBorsR} = waitForNBors(Nameservice,ID),
+	{LeftN,RightN} = getNBorsContacts(Nameservice,{NBorsL,NBorsR},ID),
+	log(ID, t_s(ID) ++ "Linker Nachbar " ++ t_s(NBorsL) ++ " gebunden."),
+	log(ID, t_s(ID) ++ "Rechter Nachbar " ++ t_s(NBorsR) ++ " gebunden."),
 	%%Wartet auf initialen Mi-Wert
 	Mi = waitForMi(Nameservice,ID),
-	loop(Mi, ID, Nameservice, CoordinatorContacts, {LeftN,RightN}, {PrGr,Team,ID,StarterID}, WorkTime,TermTime,"LogFile",0,Quota,werkzeug:reset_timer(ID, TermTime/2, vote)).
+	log(ID, t_s(ID) ++ "Mi " ++ t_s(Mi) ++ " bekommen."),
+	loop(Mi, ID, Nameservice, CoordinatorContacts, {LeftN,RightN}, {PrGr,Team,ID,StarterID}, WorkTime,TermTime,"LogFile",0,Quota,werkzeug:reset_timer(ID, trunc(TermTime/2), vote)).
 
 %%Alle Methoden ab hier wurden im Entwurf vorgegebn.
 
 %%%% loop()
-loop(Mi, ID, Nameservice, Coordinator, _, AllIDData, _,_,LogFile,VoteFlag,Quota,_) when VoteFlag == 2 ->
-	vote(Mi, ID, Nameservice, Coordinator, AllIDData, LogFile, Quota, 0);
+loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFile,2,Quota,Timer)  ->
+	Nameservice ! {erlang:self(),{multicast,vote,ID}},
+	log(ID, t_s(ID) ++ "Zeit abgelaufen! ABSTIMMUNG!"),
+	vote(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFile,0,Quota,Timer, 0);
 loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFile,VoteFlag,Quota,Timer) ->
 	receive
 		{setpm,MiNeu} ->
-			werkzeug:reset_timer(Timer, TermTime/2, vote),
+			werkzeug:reset_timer(Timer, trunc(TermTime/2), vote),
+			log(ID, t_s(ID) ++ "Mi wurde neu gesetzt: " ++ t_s(MiNeu)),
 			loop(MiNeu, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,0,Quota,Timer);
 		{sendy,Y} ->
-			werkzeug:reset_timer(Timer, TermTime/2, vote),
+			werkzeug:reset_timer(Timer, trunc(TermTime/2), vote),
 			MiNeu = calcNewMi(Y,Mi,Coordinator,NBors,ID,WorkTime),
 			loop(MiNeu, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,0,Quota,Timer);
 		{From,{vote,Initiator}} ->
@@ -46,24 +57,30 @@ loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFi
 			From ! {pongGGT, ID},
 			loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,VoteFlag,Quota,Timer);
 		vote ->
-			werkzeug:reset_timer(Timer, TermTime/2, vote),
-			loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,VoteFlag+1,Quota,Timer);
+			case VoteFlag of
+				1 ->
+					loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,2,Quota,Timer);
+				0 ->
+					werkzeug:reset_timer(Timer, trunc(TermTime/2), vote),
+					loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData,WorkTime, TermTime,LogFile,1,Quota,Timer)
+			end;			
 		kill ->
 			kill(Nameservice,ID)
 	end.
 
 %%%% vote()
-vote(Mi, ID, Nameservice, Coordinator, AllIDData, LogFile, Quota, YesQuota) when Quota == YesQuota ->
+vote(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFile,VoteFlag,Quota,Timer, YesQuota) when Quota == YesQuota ->
 	Coordinator ! {erlang:self(),briefterm,{ID,Mi,werkzeug:timeMilliSecond()}},
-	receive
-		kill ->
-			kill(Nameservice,ID)
-	end;
-vote(Mi, ID, Nameservice, Coordinator, AllIDData, LogFile, Quota, YesQuota) ->
-	Nameservice ! {erlang:self(),{multicast,vote,ID}},
+	log(ID, t_s(ID) ++ " ABSTIMMUNG ERFOLGREICH"),
+	loop(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFile,0,Quota,Timer);
+vote(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFile,VoteFlag,Quota,Timer, YesQuota) ->
 	receive
 		{voteYes, CName} ->
-			vote(Mi, ID, Nameservice, Coordinator, AllIDData, LogFile, Quota, YesQuota+1);
+			log(ID, "ABSTIMMUNG bei " ++ t_s(ID) ++ ":" ++ t_s(CName) ++ ": stimmt ab  mit >JA<! Quota bei " ++ t_s(YesQuota+1) ++ " UM:" ++ t_s(werkzeug:timeMilliSecond())),
+			vote(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFile,VoteFlag,Quota,Timer, YesQuota+1);
+		{From,{vote,Initiator}} ->
+			From ! {voteYes, ID},
+			vote(Mi, ID, Nameservice, Coordinator, NBors, AllIDData, WorkTime,TermTime,LogFile,VoteFlag,Quota,Timer, YesQuota);
 		kill ->
 			kill(Nameservice,ID)
 	end.
@@ -71,6 +88,7 @@ vote(Mi, ID, Nameservice, Coordinator, AllIDData, LogFile, Quota, YesQuota) ->
 kill(Nameservice,ID) ->
 	Nameservice ! {erlang:self(),{unbind,ID}},
 	erlang:unregister(ID),
+	log(ID, t_s(ID) ++ " hat sich beendet!"),
 	erlang:exit(erlang:self(),normal).
 %%HILFSMETHODEN%%
 
@@ -127,13 +145,16 @@ registerByCoor(CoordinatorContacts,ID) ->
 	CoordinatorContacts ! {hello, ID}.
 
 calcNewMi(Y,Mi,Coordinator,NBors,ID,WorkTime) ->
+	log(ID, "Y erhalten: " ++ t_s(Y)),
 	NewMi = calc(Y,Mi),
-	time:sleep(WorkTime),
+	timer:sleep(WorkTime),
 	case NewMi of
-		nope ->
-				Mi;
+		noop ->
+			log(ID, "Mi wurde nicht geaendert: " ++ t_s(Mi)),
+			Mi;
 		_ ->
-			newMiSend(Mi, Coordinator, NBors, ID),
+			log(ID, "Mi wurde berechnet: " ++ t_s(NewMi)),
+			newMiSend(NewMi, Coordinator, NBors, ID),
 			NewMi
 	end.
 
@@ -146,3 +167,10 @@ calc(OtherMi,SelfMI) when OtherMi >= SelfMI ->
 	noop;
 calc(OtherMi,SelfMI) ->
 	((SelfMI-1) rem OtherMi) + 1.
+
+log(ID, Message) ->
+	[_|[NodeName]] = string:tokens(lists:concat([erlang:node()]), "@"),
+	werkzeug:logging(erlang:list_to_atom(lists:concat([?LOG, "GGTP_",ID,"@",NodeName, '.log'])), Message++"\n").
+
+t_s(ToS) ->
+	werkzeug:to_String(ToS).
