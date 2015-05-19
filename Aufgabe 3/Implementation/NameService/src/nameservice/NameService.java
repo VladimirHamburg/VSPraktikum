6600170a930453ccc5c251f04b591ebacc81884e
Vladimir Malikov <v.malikov.hamburg@gmail.com>
2015-05-19 16:08:48 +0200

    Den Anfang gemacht.

---
+++ Aufgabe 3/Implementation/NameService/src/nameservice/NameService.java
@@ -0,0 +1,18 @@
+package nameservice;
+
+/**
+ *
+ * @author Vladimir
+ */
+
+
+public class NameService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
